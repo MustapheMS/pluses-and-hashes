13d26f826310d77f78dfe66ee83d2b31f01d958a
MustapheMS <moztaphem9@gmail.com>
2023-06-15 21:22:48 +0300

    Plus and Hash Done 100%

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+
+
+//Done 💯💯😊
+
+function plusHashCount(str) {
+    let hashCount = 0;
+    let plusCount = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '#') {
+        hashCount++;
+      } else if (str[i] === '+') {
+        plusCount++;
+      }
+    }
+    
+    return [hashCount, plusCount];
+  }
+  console.log(plusHashCount("+++++####"));
